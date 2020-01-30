80636945ba744e489324ea516334254bf002e385
Edwin Torres <edwin.torres.jr2@gmail.com>
2020-01-30 17:30:50 -0500

    Update chat.js

--- edogram/src/components/chat.js
+++ edogram/src/components/chat.js
@@ -1,20 +1,25 @@
 import React from "react";
 import axios from "axios";
 axios.defaults.withCredentials = true;
+const user = JSON.parse(localStorage.getItem("user"));
 
 class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      // stores messages user has sent
       sentMessages: [],
+      // stores messages user has recieved
       receivedMessages: [],
+      // this will be used to store a concatenated array of sent and received messages to be sorted later by id
       messages: [],
+      // this is used to store the body of a new message created in the form
       newMessage: ""
     };
   }
 
   componentDidMount = () => {
-    const user = JSON.parse(localStorage.getItem("user"));
+    // get messages sent by the logged in user and sets them to state
     axios
       .get(
         `http://localhost:5000/posts/${user.username}/${this.props.match.params.receiver}`
@@ -22,33 +27,52 @@ class Chat extends React.Component {
       .then(response => {
         this.setState({ sentMessages: response.data });
       })
-      .catch(err => {
-        console.log(err);
+      // below function gets received messages and sets them to state
+      .then(response => {
+        this.getReceivedMessages().catch(err => {
+          console.log(err);
+        });
       });
+  };
 
-    axios
+  getReceivedMessages = () => {
+    return axios
       .get(
         `http://localhost:5000/posts/${this.props.match.params.receiver}/${user.username}`
       )
       .then(response => {
         this.setState({ receivedMessages: response.data });
       })
+      .then(response => {
+        this.combineMessages();
+      })
       .catch(err => {
         console.log(err);
       });
   };
 
+  combineMessages = () => {
+    console.log(this.state);
+    let receivedMessages = this.state.receivedMessages;
+    let sentMessages = this.state.sentMessages;
+    let messages = sentMessages.concat(receivedMessages);
+    messages.sort(function(a, b) {
+      return a.id - b.id;
+    });
+    this.setState({ messages: messages });
+  };
+
   submitHandler = e => {
     e.preventDefault();
-    const user = JSON.parse(localStorage.getItem("user"));
+
     let message = {
       body: this.state.newMessage,
       sender: user.username,
       receiver: this.props.match.params.receiver
     };
-    console.log(message);
     axios
       .post("http://localhost:5000/posts", message)
+      // Instead, run a get request, and then combine
       .then(response => {
         this.setState(prevState => ({
           sentMessages: [...prevState.sentMessages, message]
@@ -72,7 +96,7 @@ class Chat extends React.Component {
         <h1>Chat</h1>
         <div>
           <h3>Messages:</h3>
-          {this.state.sentMessages.map(message => {
+          {this.state.messages.map(message => {
             return <div>{message.body}</div>;
           })}
           <form onSubmit={this.submitHandler}>
